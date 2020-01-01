83cf0e4e0b6995bb14a5a4150e3708f214fc97c8
shoronts <mdshariffoysalshoron@gmail.com>
2020-01-01 22:26:23 +0600

    about page 2nd section complete but 3rd page is not complete

--- assets/js/mycarousel.js
+++ assets/js/mycarousel.js
@@ -16,6 +16,25 @@
 })
 // Owl Carousel End   
 
+// about page carousel start
+
+$('.partners-logo').owlCarousel({
+    loop:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+// about page carousel end
 
 var owl = $('.owl-carousel');
 owl.owlCarousel();
